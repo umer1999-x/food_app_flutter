e56cced1a23fe085e15e02092987d08fa76c527f
umer1999-x <03-134182-087@student.bahria.edu.pk>
2022-01-06 18:13:48 +0500

    second commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,11 +10,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget SingleProduct(){
+  Widget SingleProduct() {
     return Row(
       children: [
         Container(
-          margin:  const EdgeInsets.symmetric(horizontal: 5),
+          margin: const EdgeInsets.symmetric(horizontal: 5),
           height: 230,
           width: 160,
           decoration: BoxDecoration(
@@ -61,8 +61,7 @@ class _HomePageState extends State<HomePage> {
                         width: 50,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            border:
-                            Border.all(color: Colors.yellowAccent)),
+                            border: Border.all(color: Colors.yellowAccent)),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: const [
@@ -86,8 +85,7 @@ class _HomePageState extends State<HomePage> {
                         width: 50,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            border:
-                            Border.all(color: Colors.yellowAccent)),
+                            border: Border.all(color: Colors.yellowAccent)),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: const [
@@ -102,17 +100,111 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
-              )
+              ),
             ],
           ),
-        )
+        ),
       ],
     );
   }
+
+  Widget listTile({required IconData icon, required String title}) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 32,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      drawer: const Drawer(),
+      drawer: Drawer(
+        child: Container(
+          color: const Color(0xffa28527),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 43,
+                      child: CircleAvatar(
+                          backgroundColor: Colors.red.shade400, radius: 40),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          width: 120,
+                        ),
+                        const Text('Welcome guest'),
+                        OutlinedButton(
+                          onPressed: null,
+                          child: const Text("Login"),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.red.shade400),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              listTile(icon: Icons.home_outlined, title: 'Home'),
+              listTile(icon: Icons.shop_outlined, title: 'Shop'),
+              listTile(icon: Icons.person_outlined, title: 'Profile'),
+              listTile(icon: Icons.notifications_outlined, title: 'Notification'),
+              listTile(icon: Icons.star_outlined, title: 'Ratings & star'),
+              listTile(icon: Icons.favorite_outlined, title: 'Wishlist'),
+              listTile(icon: Icons.copy_outlined, title: 'Raise a Complaint'),
+              listTile(icon: Icons.format_quote_outlined, title: 'FAQs'),
+              Container(
+                height: 350,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Contact Us'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text('Phone no.'),
+                        SizedBox(width: 8),
+                        Text('098765432122'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Text('Mail Us'),
+                        SizedBox(width: 8),
+                        Text('shkh@email.com'),
+                      ],
+                    )
+
+
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: const Color(0xffa28527),
